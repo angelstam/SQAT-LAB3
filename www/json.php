938ce735349ef3a8cf0a3ddd4dac2fe8ab21a733
Johan Angelstam <johan@angelstam.se>
2014-03-25 17:00:57 +0800

    Change json.php to use PDO.

--- www/json.php
+++ www/json.php
@@ -6,52 +6,55 @@ $pass = 'secret';
 $host = '127.0.0.1';
 $db_name = 'commissionlab';
 
-// Data Source Name: This is the universal connection string
-$dsn = "mysql://$user:$pass@$host/$db_name";
-
-// MDB::connect will return a PEAR MDB object on success
-// or an PEAR MDB Error object on error
-
-$db = MDB2::connect($dsn);
-
-// With MDB::isError you can differentiate between an error or
-// a valid connection.
-if (MDB2::isError($db)) {
-    die ($db->getMessage());
-}
-
-if (isset($_GET['target']))
+try
 {
-	if ($_GET['target'] == "salespersons")
-	{
-		$result = $db->query("SELECT * FROM salespersons");
-	}
-	else if ($_GET['target'] == "orders")
-	{
-		$result = $db->query("SELECT * FROM orders");
-	}
-	else if ($_GET['target'] == "totalSoldValue")
-	{
-		$result = $db->query("SELECT 
-			(orders.locks*prices.locks)+
-			(orders.stocks*prices.stocks)+
-			(orders.barrels*prices.barrels) as totalSoldAmount 
-			FROM orders,prices 
-			WHERE MONTH(date)=".$_GET['month']."
-			AND YEAR(date)=".$_GET['year']);
-	}
-	else if ($_GET['target'] == "commissionLevels")
-	{
-		$result = $db->query("SELECT * FROM commissionlevels");
-	}
+	// Create a new PDO to connect to the database.
+	$db = new PDO("mysql:host=$host;dbname=$db_name", $user, $pass);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
+    die('ERROR: ' . $e->getMessage());
 }
 
-// Always check that $result is not an error
-if (MDB2::isError($result)) {
-    die ($result->getMessage());
+try
+{
+	if (isset($_GET['target']))
+	{
+		if ($_GET['target'] == "salespersons")
+		{
+			$result = $db->query("SELECT * FROM salespersons");
+		}
+		else if ($_GET['target'] == "orders")
+		{
+			$result = $db->query("SELECT * FROM orders");
+		}
+		else if ($_GET['target'] == "totalSoldValue")
+		{
+			$result = $db->prepare("SELECT
+				(orders.locks*prices.locks)+
+				(orders.stocks*prices.stocks)+
+				(orders.barrels*prices.barrels) as totalSoldAmount
+				FROM orders,prices
+				WHERE MONTH(date) = :month
+				AND YEAR(date) = :year");
+			
+			$result->bindParam(':month', $_GET['month'], PDO::PARAM_INT);
+			$result->bindParam(':year', $_GET['year'], PDO::PARAM_INT);
+			$result->execute();
+		}
+		else if ($_GET['target'] == "commissionLevels")
+		{
+			$result = $db->query("SELECT * FROM commissionlevels");
+		}
+	}
+}
+catch (PDOException $e)
+{
+    die('ERROR: ' . $e->getMessage());
 }
 
-echo json_encode($result->fetchAll(MDB2_FETCHMODE_ASSOC));
+echo json_encode($result->fetchAll(PDO::FETCH_ASSOC));
 
 // close conection
 $db->disconnect();
